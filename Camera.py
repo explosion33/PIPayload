73fcea42267e349cadafc0177ff582c829854b64
Ethan Armstrong <w.ethan.armstrong@gmail.com>
2022-01-30 14:57:15 -0800

    added Camera class that needs to be tested

---
+++ Camera.py
@@ -0,0 +1,43 @@
+import imp
+import picamera
+from multiprocessing import Process, Queue
+
+class Camera:
+    def __init__(this, resolution):
+        this.camera = picamera.PiCamera()
+        this.camera.resolution = resolution
+
+    def startCamera(this, name):
+        this.camera.start_recording(name)
+
+    def startCameraTask(this, resolution, name) -> Process:
+        """
+        startCameraTask(resolution) : starts camera recording in a background process
+        resolution : tuple (w,h) resolution to record in
+        name       : String, name of video + exstension
+        returns : Process
+        """
+
+        p = Process(target=this.camera.start_recording, args=(name,))
+        p.start()
+        this.process = p
+    
+    def stopCamera(this):
+        this.camera.stop_recording()
+
+
+
+
+# successful test of this code would be to observe 0-10 printed to console, then after the the
+# program closes find an 11 second long file.
+if "__main__" in __name__:
+    import time
+    c = Camera((640,480))
+    c.startCamera('my_video.h264')
+
+    i = 0
+    while i<=10:
+        print(i)
+        i += 1
+        time.sleep(1)
+    c.stopCamera
--- main.py
+++ main.py
@@ -1,38 +1,37 @@
 import time
 from IMU import IMU
 from Logger import Logger
+from Camera import Camera
 
-def main(console):
+
+def main():
     sensors = IMU()
     sensorLog = Logger("sensors.log")
+
+    #new untested code
+    c = Camera((640,480))
+    c.startCamera('my_video.h264')
+    
+    #end
     
     while True:
-        sensorLog.log("Temp: {}; ".format(sensors.temp()))
-        sensorLog.log("Accel: {}; ".format(sensors.accel()))
-        sensorLog.log("Mag: {}; ".format(sensors.mag()))
-        sensorLog.log("Gyro: {}; ".format(sensors.gyro()))
-        sensorLog.log("Euler: {}; ".format(sensors.euler()))
-        sensorLog.log("Quaternion: {}; ".format(sensors.quaternion()))
-        sensorLog.log("Linear Accel: {}; ".format(sensors.linear_accel()))
-        sensorLog.log("Gravity: {}; ".format(sensors.gravity()))
-        sensorLog.logLine()
+        logSensors(sensorLog, sensors)
 
-        if console:
-            logToConsole(sensors)
 
         time.sleep(1)
 
 
-def logToConsole(sensors) -> None:
-    print("Temperature: {} degrees C".format(sensors.temp()))
-    print("Accelerometer (m/s^2): {}".format(sensors.accel()))
-    print("Magnetometer (microteslas): {}".format(sensors.mag()))
-    print("Gyroscope (rad/sec): {}".format(sensors.gyro()))
-    print("Euler angle: {}".format(sensors.euler()))
-    print("Quaternion: {}".format(sensors.quaternion()))
-    print("Linear acceleratison (m/s^2): {}".format(sensors.linear_accel()))
-    print("Gravity (m/s^2): {}".format(sensors.gravity()))
-    print()
+def logSensors(sensorLog, sensors):
+    sensorLog.log("Temp: {}; ".format(sensors.temp()))
+    sensorLog.log("Accel: {}; ".format(sensors.accel()))
+    sensorLog.log("Mag: {}; ".format(sensors.mag()))
+    sensorLog.log("Gyro: {}; ".format(sensors.gyro()))
+    sensorLog.log("Euler: {}; ".format(sensors.euler()))
+    sensorLog.log("Quaternion: {}; ".format(sensors.quaternion()))
+    sensorLog.log("Linear Accel: {}; ".format(sensors.linear_accel()))
+    sensorLog.log("Gravity: {}; ".format(sensors.gravity()))
+    sensorLog.logLine()
+
 
 if "__main__" in __name__:
     main(True)
