4b9bf90c6436d199dd0f804338a974458523ce99
Ethan Armstrong <w.ethan.armstrong@gmail.com>
2022-01-30 22:06:55 -0800

    added threading and some test code

--- Servo.py
+++ Servo.py
@@ -1,4 +1,8 @@
+import imp
 import RPi.GPIO as GPIO
+from multiprocessing import Process
+import time
+
 
 class Servo:
     def __init__(this, pin, hz):
@@ -8,13 +12,22 @@ class Servo:
         this.pwm.start(0)
         this.pin = pin
 
+    def setAngleTask(this, angle):
+        p = Process(target=this.setAngle, args=(angle,))
+        p.start()
+
     def setAngle(this, angle):
+        
         duty = angle/18 + 2 #this needs to be tested, its different for every servo
         GPIO.output(this.pin, True)
         this.pwm.ChangeDutyCycle(duty)
 
-        # some sort of delay here, most likely want to use processing module
-        # sleep(1)
+        time.sleep(1)
 
         GPIO.output(this.pin, False)
         this.pwm.ChangeDutyCycle(0)
+
+
+if "__main__" in __name__:
+    s = Servo(3,50)
+    s.setAngle(40)
