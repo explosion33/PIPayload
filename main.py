eea45c5e3b282e99bc85ad5d394591187b6cabca
Ethan Armstrong <w.ethan.armstrong@gmail.com>
2022-02-10 14:08:14 -0800

    blocked out what final system will look like

--- main.py
+++ main.py
@@ -2,24 +2,36 @@ import time
 from IMU import IMU
 from Logger import Logger
 from Camera import Camera
+from Servo import Servo
 
 
 def main():
     sensors = IMU()
     sensorLog = Logger("sensors.log")
 
-    #new untested code
+    s = Servo(3,50)
+    s.setAngle(90)
+
     c = Camera((640,480))
     c.startCamera('my_video.h264')
     
-    #end
-    
     while True:
+        if readyToDeploy():
+            s.setAngle(0)
+
+        sensorLog.log(time.time())
         logSensors(sensorLog, sensors)
 
+        transmitData()
 
         time.sleep(1)
 
+
+def transmitData():
+    pass
+
+def readyToDeploy():
+    return False
 
 def logSensors(sensorLog, sensors):
     sensorLog.log("Temp: {}; ".format(sensors.temp()))
