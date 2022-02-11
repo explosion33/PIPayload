a33b85b984abb47e33aafcb0e92074d32d6a057a
Ethan Armstrong <w.ethan.armstrong@gmail.com>
2022-02-10 19:56:31 -0800

    removed class

--- Servo.py
+++ Servo.py
@@ -2,49 +2,6 @@ import RPi.GPIO as GPIO
 from multiprocessing import Process, Queue
 import time
 
-def asyncSleep(t, callback):
-    time.sleep(t)
-    callback()
-
-class Servo:
-    def __init__(this, pin, hz):
-        """
-        Servo(pin, hz) : creates a new servo object\n
-        pin : PWM pin for servo communication\n
-        hz  : frequency in hertz to communicate with the servo
-        """
-        
-
-
-    def setAngle(this,angle,t=1):
-        """
-        setAngleTask(angle, time) : communicates with servo to set its angle\n
-        angle : int, the angle to set the servo\n
-        time  : time to wait to turn the servo (1s to be safe, but can be measured) 
-        """
-        if not this.isMoving:
-            this.setAngleHelper(angle,t)
-        else:
-            print("servo is currently moving")
-
-    #helper function
-    def setAngleHelper(this, angle, t=1) -> None:
-        """
-        setAngleTask(angle, time) : communicates with servo to set its angle\n
-        angle : int, the angle to set the servo\n
-        time  : time to wait to turn the servo (1s to be safe, but can be measured) 
-        """
-        this.isMoving = True
-        
-
-        this.isMoving = False
-
-        
-
-    def afterMove(this):
-        pass
-
-
 def moveServo(pin, hz, angle, t=1):
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(pin, GPIO.OUT)
@@ -66,7 +23,4 @@ def moveServo(pin, hz, angle, t=1):
 
 
 if "__main__" in __name__:
-    s = Servo(11,50)
-
-    p = Process(target=s.setAngle, args=(0,))
-    p.start()
+    pass
