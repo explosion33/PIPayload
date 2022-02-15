a70d033a8c15c160bf38d5c43acb8bf5b22bedfb
Ethan Armstrong <w.ethan.armstrong@gmail.com>
2022-02-15 14:15:40 -0800

    created new method for controlling servo

---
+++ Servo2.py
@@ -0,0 +1,30 @@
+from tkinter.messagebox import NO
+import RPi.GPIO as GPIO
+from multiprocessing import Process, Queue
+import time
+
+class Servo:
+    def __init__(this, pin, hz=50, startAngle=0):
+        this.pin = pin
+
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(pin, GPIO.OUT)
+
+        pwm = GPIO.PWM(pin, hz)
+        pwm.start(startAngle)
+    
+    def changeAngle(this, angle, t=1):
+        GPIO.output(this.pin, True)
+        duty = angle/18 + 2
+        this.pwm.ChangeDutyCycle(duty)
+
+    
+    def disableGpioAfter(t, pin):
+        time.sleep(t)
+        GPIO.output(pin, False)
+
+
+if "__main__" in __name__:
+    s = Servo(11)
+    s.changeAngle(90)
+    
