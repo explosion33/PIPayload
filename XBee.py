ee6f8353eb10144a2cd9286ae3e29c7b23401c85
Ethan Armstrong <w.ethan.armstrong@gmail.com>
2022-02-18 17:02:29 -0800

    changes to library plus comments

--- XBee.py
+++ XBee.py
@@ -1,10 +1,18 @@
-from digi.xbee.devices import DigiMeshDevice, RemoteDigiMeshDevice, XBee64BitAddress
+from digi.xbee.devices import DigiPointDevice, RemoteDigiPointDevice, XBee64BitAddress
  
 
-device = DigiMeshDevice("/dev/serial0", 9600)
+#transmitting XBee should be in coordinader mode
+#should be set to API mode aswell
+
+#reciever should be in router mode
+#can be either API or AT depending on if using XTCU or python API
+
+
+#make sure to scan for device, using portscan.sh script to get the right /dev/...
+device = DigiPointDevice("/dev/serial0", 9600)
 device.open()
 
-remote_device = RemoteDigiMeshDevice(device, XBee64BitAddress.from_hex_string("13A20041C7BFFC"))
+remote_device = RemoteDigiPointDevice(device, XBee64BitAddress.from_hex_string("13A20041C7BFFC")) #MAC adress of other Xbee
 
 while True:
     device.send_data(remote_device, "test data")
