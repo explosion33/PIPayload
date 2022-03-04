69d80154cf12825105621d85e9092a0f87977e24
Ethan Armstrong <w.ethan.armstrong@gmail.com>
2022-03-03 17:04:37 -0800

    added stylized graph view for the api data

--- ground/static/vis.js
+++ ground/static/vis.js
@@ -1,37 +1,61 @@
 function rand() { 
     return Math.random();
 }
-  
-Plotly.plot('graph', [{
-    x: [],
-    y: [],
-    mode: 'lines',
-    line: {color: '#80CAF6'}
-}]);
 
-var cnt = 0;
+function dynamicGraph(div, apiEnd, updateTime, title) {
+    let layout = {
+        title: {
+            text: title
+        }
+    };
 
-//some api / serial data request
-async function getNewData() {
-    var res = await fetch("/getData"+cnt)
+    let data = [{
+        x: [],
+        y: [],
+        mode: 'lines',
+        line: {color: '#80CAF6'}
+    }];
+
+    Plotly.plot(div, data, layout);
+
+    graph = {
+        div : div,
+        url : apiEnd,
+        numPoints : 0, 
+    };
+
+    window.setInterval(updateGraph, updateTime, graph);
+
+    return graph
+    
+}
+
+
+//some api / serial data request "/getData"+cnt
+async function getNewData(graph) {
+    var res = await fetch(graph.url + graph.numPoints)
     var d   = await res.json()
             
     var data = []
     for (let i = 0; i<d.length; i++) {
-        cnt += 1;
+        graph.numPoints += 1;
         data[i] = {x: d[i][0], y: d[i][1]};
     }
 
     return data;
 }
 
-var updateGraph = async function() {
-    var data = await getNewData()
+var updateGraph = async function(graph) {
+    var data = await getNewData(graph)
 
     data.forEach(point => {
-        Plotly.extendTraces('graph', {y: [[point.y]], x: [[point.x]]}, [0])
+        Plotly.extendTraces(graph.div, {y: [[point.y]], x: [[point.x]]}, [0])
     });
 }
 
+data = [["/api/accelX/", "accelX", "Acceleration X vs Time"],["/api/accelY/", "accelY", "Acceleration Y vs Time"],["/api/accelZ/", "accelZ", "Acceleration Z vs Time"]]
+graphs = []
 
-var intervalId = window.setInterval(updateGraph, 1000);
+for (let i = 0; i<data.length; i++) {
+    graphs[i] = dynamicGraph(data[i][1], data[i][0], 1000, data[i][2])
+}
